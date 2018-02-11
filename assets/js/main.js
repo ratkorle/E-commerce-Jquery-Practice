2c9459b86ae9dcd611598e727a23a676d955b441
Ratko Korle <ratko_korle@hotmail.com>
2018-02-11 21:02:53 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    $('.login-button').on('click', () => {
+    $('.login-form').toggle();
+});
+
+$('.menu-button').on('mouseenter', () => {
+    $('.nav-menu').show();
+$('.menu-button').addClass('button-active');
+$('.nav-menu').removeClass('hide');
+
+$('.menu-button').animate({
+    fontSize: '24px'
+}, 200)
+})
+
+$('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide();
+$('.menu-button').css({
+    color: '#EFEFEF',
+    backgroundColor: '#303030'
+})
+});
